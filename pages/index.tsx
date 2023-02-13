36774617b445d6ba014fb773ee6a1bce089b7c54
Andrey <dovban.andreyy@rambler.ru>
2023-02-13 13:35:12 +0300

    добавлены глобальные стили

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,12 @@ import withLayout from '../Layout/Layout';
 import styles from '../styles/Home.module.css';
 
 function Home() {
-    return <div className={styles.home}>weather-map</div>;
+    return (
+        <div className={styles.home}>
+            weather-map
+            <button>click</button>
+            <a href='#'>LINK</a>
+        </div>
+    );
 }
 export default withLayout(Home);
