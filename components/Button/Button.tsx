ca2ac6ed0f3779b2a12ec3db9a8771d5fb089234
Andrey <dovban.andreyy@rambler.ru>
2023-02-14 07:02:34 +0300

    стилизация кнопки

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -5,7 +5,6 @@ export function Button({ children, ...props }: ButtonProps): JSX.Element {
     function mov(e) {
         const x = e.pageX - e.target.offsetLeft;
         const y = e.pageY - e.target.offsetTop;
-        console.log(e.target.style.cssText);
 
         e.target.style.setProperty('--x', `${x}px`);
         e.target.style.setProperty('--y', `${y}px`);
@@ -13,7 +12,6 @@ export function Button({ children, ...props }: ButtonProps): JSX.Element {
 
     return (
         <button {...props} className={styles.button} onMouseMove={mov}>
-            <span className={styles.span}></span>
             {children}
         </button>
     );
