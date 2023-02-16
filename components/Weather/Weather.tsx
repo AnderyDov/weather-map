368533e075a558673a035be46d4553f6b77c3eaf
Andrey <dovban.andreyy@rambler.ru>
2023-02-16 13:51:46 +0300

    начата работа с блоком weather

--- components/Weather/Weather.tsx
+++ components/Weather/Weather.tsx
@@ -6,6 +6,9 @@ import Arrow from './arrow.svg';
 import Compass from './compass.svg';
 import { getSunTime, getIcon, windDirection } from '../../helpers';
 import cn from 'classnames';
+import Drop from './drop.svg';
+import Wind from './wind.svg';
+import Pressure from './pressure.svg';
 
 export function Weather({ ...props }: WeatherProps): JSX.Element {
     const city = useRecoilValue<string>(cityState);
@@ -22,44 +25,38 @@ export function Weather({ ...props }: WeatherProps): JSX.Element {
                 [styles.hide]: city.trim() === '',
             })}
         >
-            <div>Населённый пунт</div>
-            <div>
-                {weather.name}
-                {'  '}
-                <span className={styles.icon}>
-                    {getIcon(weather.weather[0].icon)}
-                </span>
-            </div>
-            <div>Условия</div>
-            <div>{weather.weather[0].description}</div>
-            <div>Координаты</div>
-            <div>
-                {weather.coord.lat} {weather.coord.lon}
-            </div>
-            <div>Температура</div>
-            <div>{weather.main.temp} С&deg;</div>
-            <div>Ощущается как</div>
-            <div>{weather.main.feels_like} С&deg;</div>
-            <div>Давление</div>
-            <div>{weather.main.pressure} гПа</div>
-            <div>Влажность</div>
-            <div>{weather.main.humidity} %</div>
-            <div>Восход</div>
-            <div>{sunrise}</div>
-            <div>Закат</div>
-            <div>{sunset}</div>
-            <div>Скорость ветра</div>
-            <div>{weather.wind.speed} м/с</div>
-            <div>Порывы ветра</div>
-            <div>{weather.wind.gust} м/с</div>
-            <div>Направление ветра {windDirection(weather.wind.deg)}</div>
-            <div className={styles.wrap}>
+            <div className={styles.name}>{weather.name}</div>
+            {/* <div className={styles.wrap}>
                 <Compass className={styles.compass} />
                 <Arrow
                     className={styles.arrow}
                     style={{ rotate: `${weather.wind.deg}deg` }}
                 />
+            </div> */}
+            <div className={styles.temp}>{weather.main.temp}&deg;</div>
+            <div className={styles.icon}>
+                {getIcon(weather.weather[0].icon)}
             </div>
+            <div className={styles.description}>
+                {weather.weather[0].description}
+            </div>
+            <div className={styles.feel}>
+                ощущается&nbsp;как&nbsp;{weather.main.feels_like}&deg;
+            </div>
+            <div className={styles.speed}>
+                <Wind className={styles.icon_min} />
+                {weather.wind.speed}&nbsp;м/с
+            </div>
+            <div className={styles.humidity}>
+                <Drop className={styles.icon_min} />
+                {weather.main.humidity}&nbsp;%
+            </div>
+            <div className={styles.pressure}>
+                <Pressure className={styles.icon_min} />
+                {weather.main.pressure}&nbsp;гПа
+            </div>
+            <div className={styles.sunrise}>Восход&nbsp;{sunrise}</div>
+            <div className={styles.sunset}>Закат&nbsp;{sunset}</div>
         </div>
     );
 }
